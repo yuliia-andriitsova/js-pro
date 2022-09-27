2ef390fc8d3c39086525730439852ec73e6be4c1
Ruslan Volovenko <vokoloven@gmail.com>
2022-09-27 21:47:55 +0300

    update

--- src/js/day-night-theme.js
+++ src/js/day-night-theme.js
@@ -1,3 +1,20 @@
-const refs = {
+const refsTheme = {
   body: document.querySelector('body'),
+  themeBox: document.querySelector(
+    'body > header > div > div.header__color-theme-box > div'
+  ),
+  themeBall: document.querySelector(
+    'body > header > div > div.header__color-theme-box > div > div'
+  ),
 };
+
+refsTheme.themeBox.addEventListener('click', changeThemeOnClick);
+
+function changeThemeOnClick() {
+  const settings = {
+    settings: `refsTheme.themeBall.classList.toggle('ball-right')`,
+  };
+  localStorage.setItem('settings', JSON.stringify(settings));
+}
+
+const savedSettings = localStorage.getItem('settings');
