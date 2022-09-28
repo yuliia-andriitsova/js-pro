17d73f9af3579fc886407b3b82714a6818df9545
Ruslan Volovenko <vokoloven@gmail.com>
2022-09-28 12:18:47 +0300

    Update

--- src/js/day-night-theme.js
+++ src/js/day-night-theme.js
@@ -11,7 +11,15 @@ const refsTheme = {
   ),
   paragraphText: document.querySelectorAll('p'),
 
-  headerText: document.querySelector('h1'),
+  headerTextPr: document.querySelector('h1'),
+  headerTextSec: document.querySelectorAll('h2'),
+  headerTextQua: document.querySelectorAll('h3'),
+  textLight: document.querySelector(
+    'body > header > div > div.header__color-theme-box > span.header__color-theme-box-text.header__color-theme-box-text--light'
+  ),
+  textDark: document.querySelector(
+    'body > header > div > div.header__color-theme-box > span.header__color-theme-box-text.header__color-theme-box-text--dark'
+  ),
 };
 
 refsTheme.themeBox.addEventListener('click', changeThemeOnClick);
@@ -40,21 +48,20 @@ if (getParsedSettings.toddler === true) {
 
 function dayTheme() {
   refsTheme.body.style.backgroundColor = '#ffffff';
-  refsTheme.themeBall.style.backgroundColor = '#ffffff';
-  refsTheme.themeBox.style.backgroundColor = '#fd5103';
+  refsTheme.themeBall.style.backgroundColor = '#fd5103';
+  refsTheme.themeBox.style.backgroundColor = '#ffffff';
   refsTheme.searchInput.style.backgroundColor = '#ffffff';
-  for (let i = 0; i < refsTheme.paragraphText.length; i += 1) {
-    refsTheme.paragraphText[i].style.color = '#202025';
-  }
+  refsTheme.themeBox.style.borderColor = '#fd5103';
+  refsTheme.textLight.style.color = '#fd5103';
+  refsTheme.textDark.style.color = '#202025';
 }
 
 function nightTheme() {
   refsTheme.body.style.backgroundColor = '#202025';
-  refsTheme.themeBall.style.backgroundColor = '#fd5103';
-  refsTheme.themeBox.style.backgroundColor = '#ffffff';
-  refsTheme.themeBox.style.borderColor = '#fd5103';
+  refsTheme.themeBall.style.backgroundColor = '#ffffff';
+  refsTheme.themeBox.style.backgroundColor = '#fd5103';
+  refsTheme.themeBox.style.borderColor = '#ffffff';
   refsTheme.searchInput.style.backgroundColor = '#202025';
-  for (let i = 0; i < refsTheme.paragraphText.length; i += 1) {
-    refsTheme.paragraphText[i].style.color = '#ffffff';
-  }
+  refsTheme.textLight.style.color = '#ffffff';
+  refsTheme.textDark.style.color = '#fd5103';
 }
